1438d89ed5d06dd066080d50982803c0651a4693
Dwayne Dyer <iStagnate@gmail.com>
2021-10-07 20:20:28 -0400

    add a comment

--- src/main/java/com/istagnate/ambrose/Ambrose.java
+++ src/main/java/com/istagnate/ambrose/Ambrose.java
@@ -23,6 +23,7 @@ import java.util.stream.Collectors;
 public class Ambrose {
     public static final String MOD_ID = "ambrose";
     // Directly reference a log4j logger.
+    // added a comment
     private static final Logger LOGGER = LogManager.getLogger();
 
     public Ambrose() {
